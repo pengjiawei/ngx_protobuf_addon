4cdbca506c6740e5cf1c1cc34f1191092bfbe437
pengjiawei <917527280@qq.com>
2018-05-02 17:59:06 +0800

    fix map

--- ngx_query_proto/ngx_http_query_module.cpp
+++ ngx_query_proto/ngx_http_query_module.cpp
@@ -7,60 +7,60 @@ extern "C" {
 
 #include <stdio.h>
 #include <stdlib.h>
-static char *
-ngx_http_query(ngx_conf_t *cf, ngx_command_t *cmd, void *conf);
+    static char *
+    ngx_http_query(ngx_conf_t *cf, ngx_command_t *cmd, void *conf);
 
-static ngx_int_t ngx_http_query_handler(ngx_http_request_t *r);
+    static ngx_int_t ngx_http_query_handler(ngx_http_request_t *r);
 
 }
 using namespace std;
 
 static ngx_command_t ngx_http_query_commands[] =
-        {
+{
 
-                {
-                        ngx_string("query"),
-                        NGX_HTTP_MAIN_CONF | NGX_HTTP_SRV_CONF | NGX_HTTP_LOC_CONF | NGX_HTTP_LMT_CONF |
-                        NGX_CONF_NOARGS,
-                        ngx_http_query,
-                        NGX_HTTP_LOC_CONF_OFFSET,
-                        0,
-                        NULL
-                },
+    {
+        ngx_string("query"),
+        NGX_HTTP_MAIN_CONF | NGX_HTTP_SRV_CONF | NGX_HTTP_LOC_CONF | NGX_HTTP_LMT_CONF |
+        NGX_CONF_NOARGS,
+        ngx_http_query,
+        NGX_HTTP_LOC_CONF_OFFSET,
+        0,
+        NULL
+    },
 
-                ngx_null_command
-        };
+    ngx_null_command
+};
 
 static ngx_http_module_t ngx_http_query_module_ctx =
-        {
-                NULL,                              /* preconfiguration */
-                NULL,                       /* postconfiguration */
+{
+    NULL,                              /* preconfiguration */
+    NULL,                       /* postconfiguration */
 
-                NULL,                              /* create main configuration */
-                NULL,                              /* init main configuration */
+    NULL,                              /* create main configuration */
+    NULL,                              /* init main configuration */
 
-                NULL,                              /* create server configuration */
-                NULL,                              /* merge server configuration */
+    NULL,                              /* create server configuration */
+    NULL,                              /* merge server configuration */
 
-                NULL,                   /* create location configuration */
-                NULL                    /* merge location configuration */
-        };
+    NULL,                   /* create location configuration */
+    NULL                    /* merge location configuration */
+};
 
 ngx_module_t ngx_http_query_module =
-        {
-                NGX_MODULE_V1,
-                &ngx_http_query_module_ctx,           /* module context */
-                ngx_http_query_commands,              /* module directives */
-                NGX_HTTP_MODULE,                       /* module type */
-                NULL,                                  /* init master */
-                NULL,                                  /* init module */
-                NULL,                                  /* init process */
-                NULL,                                  /* init thread */
-                NULL,                                  /* exit thread */
-                NULL,                                  /* exit process */
-                NULL,                                  /* exit master */
-                NGX_MODULE_V1_PADDING
-        };
+{
+    NGX_MODULE_V1,
+    &ngx_http_query_module_ctx,           /* module context */
+    ngx_http_query_commands,              /* module directives */
+    NGX_HTTP_MODULE,                       /* module type */
+    NULL,                                  /* init master */
+    NULL,                                  /* init module */
+    NULL,                                  /* init process */
+    NULL,                                  /* init thread */
+    NULL,                                  /* exit thread */
+    NULL,                                  /* exit process */
+    NULL,                                  /* exit master */
+    NGX_MODULE_V1_PADDING
+};
 
 
 static char *
